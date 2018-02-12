050314545719f1c8ba6657c14918c09ed85361af
Rustam <guseynov.r.v@gmail.com>
2018-02-12 18:24:56 +0300

    Better conversations

--- admin.py
+++ admin.py
@@ -26,36 +26,37 @@ def publish_request(bot: Bot, update: Update):
 
 def publish_url(bot: Bot, update: Update, user_data):
     # todo check if url is correct
+    user_data['article_url'] = update.message.text
     chats = list(map(lambda x: [x], channels))
     chats.append(["Cancel"])
     markup = ReplyKeyboardMarkup(chats, one_time_keyboard=True)
-    bot.send_message(chat_id=update.message.chat_id, text="Спасибо за урл! Укажите чат:", reply_markup=markup)
+    bot.send_message(chat_id=update.message.chat_id, text="В какой чат отправить?", reply_markup=markup)
     return ChooseChats
 
 
 def publish_chats(bot: Bot, update: Update, user_data):
     channel = update.message.text
 
-    bot.send_message(chat_id=update.message.chat_id, text="You selected {}".format(channel))
     if channel not in channels:
-        bot.send_message(chat_id=update.message.chat_id, text="No such channel")
+        bot.send_message(chat_id=update.message.chat_id, text="Публикация отменена!")
         return ConversationHandler.END
     else:
-        bot.send_message(chat_id=update.message.chat_id, text="Such channel")
         options = [["Yes"], ["No"]]
-        bot.send_message(chat_id=update.message.chat_id, text="PPublish?",
+        bot.send_message(chat_id=update.message.chat_id, text="Отправить статью")
+        bot.send_message(chat_id=update.message.chat_id, text=user_data['article_url'])
+        bot.send_message(chat_id=update.message.chat_id, text="В чат {}?".format(channel),
                          reply_markup=ReplyKeyboardMarkup(options, one_time_keyboard=True))
         return PublishOrCancel
-
-
-def publish_cancel(bot: Bot, update: Update, user_data):
-    bot.send_message(chat_id=update.message.chat_id, text="Won't publish")
-    return ConversationHandler.END
 
 
 def publish_final(bot: Bot, update: Update, user_data):
     bot.send_message(chat_id=update.message.chat_id, text="Publish: {}".format(update.message.text))
     if update.message.text == "Yes":
         bot.send_message(chat_id=update.message.chat_id, text="Published")
+    return ConversationHandler.END
+
+
+def publish_cancel(bot: Bot, update: Update, user_data):
+    bot.send_message(chat_id=update.message.chat_id, text="Won't publish")
     return ConversationHandler.END
 
